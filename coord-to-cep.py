4e34c76fb84179a57a0becdf4baf5e7e838828c0
Diego <diego.giovanini4@gmail.com>
2021-05-02 18:57:28 -0300

    Convert coordinates to address and cep

--- coord-to-cep.py
+++ coord-to-cep.py
@@ -1,1 +1,11 @@
-#teste
+import pycep_correios
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim(user_agent="coord-to-cep")
+
+location = geolocator.reverse("-23.677506, -46.540665")
+
+address1 = location.address.split(",")
+address2 = pycep_correios.get_address_from_cep(address1[8])
+
+print(address2['logradouro'] + ", " + address2['cidade'] + " - " + address2['bairro'] + " - " + address1[8])
