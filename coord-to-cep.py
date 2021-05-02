5e40edf7f984892aad9f6f56631d5df66a3cc937
Diego <diego.giovanini4@gmail.com>
2021-05-02 20:18:43 -0300

    Import and export from csv file

--- coord-to-cep.py
+++ coord-to-cep.py
@@ -1,11 +1,26 @@
-import pycep_correios
+import pandas as pd
 from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+
+def get_cep(address):
+    location = address.split(",")
+    return location[len(location)-2]
+
+def get_address(location):
+    return location.address
+
+path = input("Insert the file path, without file name: ")
+filename = input("Insert the file full name: ")
+
+df = pd.read_csv(path + '\\' + filename, sep='\t')
+#df = pd.DataFrame({'COORDS': ['-23.538918, -46.793696', '-23.478389, -46.864268', '-23.514244, -46.445519']})
 
 geolocator = Nominatim(user_agent="coord-to-cep")
+location = RateLimiter(geolocator.reverse, min_delay_seconds=1)
 
-location = geolocator.reverse("-23.677506, -46.540665")
+df['LOCATION'] = df['COORDS'].apply(location)
+df['ADDRESS'] = df['LOCATION'].apply(get_address)
+df['CEP'] = df['ADDRESS'].apply(get_cep)
+df.pop('LOCATION')
 
-address1 = location.address.split(",")
-address2 = pycep_correios.get_address_from_cep(address1[8])
-
-print(address2['logradouro'] + ", " + address2['cidade'] + " - " + address2['bairro'] + " - " + address1[8])
+df.to_csv(path + '\EXPORT_CEPS.CSV', index = False)
